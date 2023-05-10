0a901977997e2ae81f4261a95a72c64bb2761da7
canna <wyjyok@naver.com>
2023-05-10 15:48:36 +0900

    [Feat] TapView

---
+++ MC2/View/TapView/TapViewMain.swift
@@ -0,0 +1,31 @@
+
+import SwiftUI
+
+struct TapViewMain: View {
+    @State private var tag: Int = 0
+    
+    var body: some View {
+           ZStack {
+                  switch tag {
+                  case 0:
+                      TapView(tag: $tag)
+                  case 1:
+                      TapView1(tag: $tag)
+                  case 2:
+                      TapView2(tag: $tag)
+
+                  default:
+                      TapView(tag: $tag)
+                  }
+
+              }
+
+    }
+
+}
+
+struct TapViewMain_Previews: PreviewProvider {
+    static var previews: some View {
+        TapViewMain()
+    }
+}
